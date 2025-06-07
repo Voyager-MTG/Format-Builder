c488b15d8a804d7abfbe93bd7ec812520291ad10
uiwow7 <Voyager.CustomMTG@gmail.com>
2025-06-07 13:49:17 -0700

    Create build.py

---
+++ build.py
@@ -0,0 +1,103 @@
+from enum import Enum
+import os, sys, shutil
+
+path = {
+    "egg_hub_export" : "C:/Users/aydin/Documents/repos/Voyager-Indexes/egg-hub-exports/",
+    "trice_export"   : "C:/Users/aydin/Documents/repos/Voyager-Indexes/trice-exports/",
+    "egg_hub"        : "C:/Users/aydin/Documents/repos/Voyager-MTG.github.io",
+    "egg_hub_sets"   : "C:/Users/aydin/Documents/repos/Voyager-MTG.github.io/sets",
+    "trice_sets"     : "C:/Users/aydin/Documents/repos/Voyager-Field-Builder/sets/",
+    "trice_merged"   : "C:/Users/aydin/Documents/repos/Voyager-Field-Builder/export/",
+    "field_builder"  : "C:/Users/aydin/Documents/repos/Voyager-Field-Builder/",
+    "voyager_data"   : "C:/Users/aydin/Documents/repos/Voyager/tricedata/"
+}
+
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+    
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    # Is the error an access error?
+    if not os.access(path, os.W_OK):
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            # print("copying", s, d)
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+# region Trice
+
+print("BUILDING TRICE FILES\n")
+print("Copying data from trice-export")
+try:
+    shutil.rmtree(path["trice_sets"])
+except Exception as e:
+    print(e)
+
+
+try:
+    shutil.copytree(path["trice_export"], path["trice_sets"])
+except Exception as e:
+    print(e)
+
+print("Running trice-merge\n------------------------------")
+os.chdir(path["field_builder"])
+os.system(f"python trice-merge/merge.py")
+print("------------------------------")
+
+print("Moving files to Voyager")
+
+try:
+    shutil.rmtree(path["voyager_data"])
+except Exception as e:
+    print(e)
+
+
+try:
+    shutil.copytree(path["trice_merged"], path["voyager_data"])
+except Exception as e:
+    print(e)
+
+#region Egg Hub
+
+print("BUILDING EGG HUB\n")
+print("Copying files from egg-hub-export")
+
+os.chdir(path["egg_hub"])
+try: 
+    shutil.rmtree("sets")
+except:
+    print("removed sets!")
+
+try:
+    copytree(path["egg_hub_export"], "sets")
+except Exception as e:
+    print(e)
+
+try:
+    copytree(path["egg_hub_export"], "sets")
+except Exception as e:
+    print(e)
+
+print("Running build_site\n------------------------------")
+os.chdir(path["egg_hub"])
+os.system("python scripts/build_site.py")
+print("------------------------------\n")
+
+print("Built Voyager!")
